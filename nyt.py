a94a5d5590fdddd95055de6e33befc64466f7a4f
bigeast <dadongking@gmail.com>
2017-07-26 12:36:26 +0800

    [Update] write to file.

--- nyt.py
+++ nyt.py
@@ -1,11 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 from colorama import Fore, Back, Style
+
 def _getBS_NYT(bs_url):
     r = requests.get(bs_url)
     soup = BeautifulSoup(r.text, 'lxml')
     books = soup.findAll('article', class_='book')
     idx = 0
+    res = ""
     for item in books:
         idx += 1
         freshness = item.findAll('p', class_='freshness')[0].text
@@ -15,23 +17,17 @@ def _getBS_NYT(bs_url):
         publisher = item.findAll('p', class_='publisher')[0].text
         description = item.findAll('p', class_='description')[0].text.strip()
         isbn = item.findAll('meta', {'itemprop': 'isbn'})[1]['content']
-        print('#{}|{:^50}|'.format(idx, FCname) + '\t' + Fore.RED + Style.DIM + author + Style.RESET_ALL + ' ISBN: ' + isbn + Style.BRIGHT + ' ' + freshness + Style.RESET_ALL)
-        print('\t' + Fore.YELLOW + description + Style.RESET_ALL)
-        print('\n')
+        # print('#{}|{:^50}|'.format(idx, FCname) + '\t' + Fore.RED + Style.DIM + author + Style.RESET_ALL + ' ISBN: ' + isbn + Style.BRIGHT + ' ' + freshness + Style.RESET_ALL)
+        # print('\t' + Fore.YELLOW + description + Style.RESET_ALL)
+        # print('\n')
+        res += '#{}|{:^50}|'.format(idx, FCname) + '\t' + Fore.RED + Style.DIM + author + Style.RESET_ALL + ' ISBN: ' + isbn + ' ' + Style.BRIGHT + freshness + Style.RESET_ALL + '\n'
+        res += '\t' + Fore.YELLOW + description + Style.RESET_ALL + '\n\n\n'
+    return res
 
+L = ['combined-print-and-e-book-nonfiction','combined-print-and-e-book-fiction','hardcover-fiction','hardcover-nonfiction','paperback-nonfiction']
 
-def getBS_NYT_PrintEbookNonfiction():
-    bs_url = 'https://www.nytimes.com/books/best-sellers/combined-print-and-e-book-nonfiction'
-    _getBS_NYT(bs_url)
-def getBS_NYT_PrintEbookFiction():
-    bs_url = 'https://www.nytimes.com/books/best-sellers/combined-print-and-e-book-fiction'
-    _getBS_NYT(bs_url)
-def getBS_NYT_HardcoverFiction():
-    bs_url = 'https://www.nytimes.com/books/best-sellers/hardcover-fiction'
-    _getBS_NYT(bs_url)
-def getBS_NYT_HardcoverNonfiction():
-    bs_url = 'https://www.nytimes.com/books/best-sellers/hardcover-nonfiction'
-    _getBS_NYT(bs_url)
-def getBS_NYT_PaperbackNonfiction():
-    bs_url = 'https://www.nytimes.com/books/best-sellers/paperback-nonfiction'
-    _getBS_NYT(bs_url)
+for item in L:
+    bs_url = 'https://www.nytimes.com/books/best-sellers/' + item
+    res = _getBS_NYT(bs_url)
+    with open('nyt_' + item + '.txt', 'w', encoding='utf-8') as f:
+        f.write(res)
